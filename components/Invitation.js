6f6ed4d036039075b763c05255eab414fbcfa069
walwoodr <walwoodr@gmail.com>
2017-05-22 11:23:20 -0500

    Done.

---
+++ components/Invitation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Invitation extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        {this.props.children}
+      </div>
+    );
+  }
+};
+
+export default Invitation;
---
+++ components/ThemedDecorations.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class ThemedDecorations extends React.Component {
+  render() {
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      });
+    });
+
+    return (
+      <div>
+        {childrenWithExtraProp}
+      </div>
+    )
+  };
+};
